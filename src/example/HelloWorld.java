d9745ffe80de10d66a7e229815861db07b6878ae
Adams1986 <simonadams86@gmail.com>
2015-10-17 11:28:54 +0200

    Initial commit

---
+++ src/example/HelloWorld.java
@@ -0,0 +1,50 @@
+package example;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+
+/**
+ * Created by ADI on 16-10-2015.
+ */
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/helloworld")
+public class HelloWorld {
+    // The Java method will process HTTP GET requests
+    @GET
+    // The Java method will produce content identified by the MIME Media type "text/plain"
+    @Produces("text/plain")
+    public String getClichedMessage() {
+        // Return some cliched textual content
+        return "Hello World";
+    }
+
+    @Path("/user1")
+    @GET
+    @Produces("application/json")
+    public String getUser(){
+
+        User user = new User(1, "Simon", "Adams", "Adi", "12345");
+
+        System.out.println(user.toJson());
+
+        return user.toJson();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServerFactory.create("http://localhost:998/");
+        server.start();
+
+        System.out.println("Server running");
+        System.out.println("Visit: http://localhost:998/helloworld/user1");
+        System.out.println("Hit return to stop...");
+        System.in.read();
+        System.out.println("Stopping server");
+        server.stop(0);
+        System.out.println("Server stopped");
+    }
+}
---
+++ src/example/HelloWorldClient.java
@@ -0,0 +1,8 @@
+package example;/**
+ * Created by ADI on 16-10-2015.
+ */
+public class HelloWorldClient {
+  public static void main(String[] argv) {
+
+  }
+}
